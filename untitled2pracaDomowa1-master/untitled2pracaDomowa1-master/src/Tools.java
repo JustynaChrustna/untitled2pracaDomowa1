bda7f801c8e9be2ea95ef426a628c459fd661c9e
Justyna Chrustna <justyna.chrustna@gmail.com>
2018-06-02 11:39:33 +0200

    Add files via upload

--- untitled2pracaDomowa1-master/untitled2pracaDomowa1-master/src/Tools.java
+++ untitled2pracaDomowa1-master/untitled2pracaDomowa1-master/src/Tools.java
@@ -1,4 +1,61 @@
 
 public class Tools {
+	
+	
+	
+public int[][] histogram(int[][] tabIn, int max){
+	int[] tabV = new int[max];
+    int[] tabF = new int[max];
+    int indexOfLastFrequency = 0;
+
+    for (int i = 0; i < tabIn.length; i++) {
+        for (int k = 0; k <tabIn[i].length ; k++) {
+            int value = tabIn[i][k];
+
+
+            boolean valueExist = false;
+
+            for (int j = 0; j < tabV.length; j++) {
+
+                if (tabV[j] == value) {
+                    tabF[j] = tabF[j] + 1;
+                    valueExist = true;
+                }
+            }
+            if (!valueExist) {
+
+                tabV[indexOfLastFrequency] = value;
+                tabF[indexOfLastFrequency] = 1;
+                indexOfLastFrequency++;
+
+
+            }
+        }}
+
+
+    for (int i = 0; i < indexOfLastFrequency; i++) {
+        System.out.println("Wartość:" + tabV[i] + " występuje " + tabF[i]+"razy");
+
+    }
+    for (int i = 0; i < indexOfLastFrequency; i++) {
+        System.out.print(tabV[i]+" ");
+        for (int j = 0; j <tabF[i] ; j++) {
+            System.out.print("*");
+
+        }
+        System.out.println();
+
+    }
+    int[][] resultTab= {tabV, tabF};
+    return resultTab;
+
+
+}
+
+
+
+
+
+
 
 }
